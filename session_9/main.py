23e7632f78b18a8366ac48659a45946120ac94e4
him patel <himanshupatel8999@gmail.com>
2023-07-28 16:28:01 +0530

    completed

--- session_9/main.py
+++ session_9/main.py
@@ -2,3 +2,11 @@ import hello
 hello.hello()
 print(hello.RED)
 obj = hello.Hello()
+
+
+import math_operations
+
+print(math_operations.add(10, 20)) # 30
+print(math_operations.subtract(10, 20)) # -10
+print(math_operations.multiply(10, 20)) # 200
+print(math_operations.divide(10, 20)) # 0.5
